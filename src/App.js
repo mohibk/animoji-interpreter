ffaa1755711acf20287424f0fb024f7178093277
Mohib Khan <mohibk.in@gmail.com>
2021-06-10 21:40:49 +0530

    Added animoji interpreter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  const [meaning, setMeaning] = useState("");
+
+  const emojiDictionary = {
+    "🐅": "Tiger",
+    "🐆": "Leopard",
+    "🐴": "Horse Face",
+    "🐎": "Horse",
+    "🦄": "Unicorn",
+    "🦓": "Zebra",
+    "🦌": "Deer",
+    "🐮": "Cow Face",
+    "🐂": "Ox",
+    "🐃": "Water Buffalo",
+    "🐄": "Cow",
+  };
+
+  const emojis = Object.keys(emojiDictionary);
+
+  const emojiInputHandler = ({ target }) => {
+    const { value: inputEmoji } = target;
+    setUserInput(inputEmoji);
+
+    if (inputEmoji === "") {
+      setMeaning("");
+    } else if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("We don't have that emoji in our database");
+    }
+  };
+
+  const emojiClickHandler = (emoji) => {
+    setMeaning(emojiDictionary[emoji]);
+    setUserInput("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>animoji outtt!</h1>
+      <input
+        placeholder="put an emoji here"
+        id="emoji-input"
+        value={userInput}
+        onChange={emojiInputHandler}
+        type="text"
+      />
+
+      <p>{meaning}</p>
+
+      <div className="emojis">
+        {emojis.map((emoji) => (
+          <span
+            className="emoji"
+            key={emoji}
+            onClick={() => emojiClickHandler(emoji)}
+          >
+            {emoji}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
